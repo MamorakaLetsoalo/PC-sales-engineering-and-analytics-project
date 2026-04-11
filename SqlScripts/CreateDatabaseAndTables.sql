00c97d686f9d8824a9f709ad8a8d6986fa119eab
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-11 11:13:15 +0200

    create dim location table

--- SqlScripts/CreateDatabaseAndTables.sql
+++ SqlScripts/CreateDatabaseAndTables.sql
@@ -10,3 +10,11 @@ CREATE TABLE [ComputerStg].[dbo].[dim_customer](
 	[Customer_Contact_Number] [nvarchar](50) NOT NULL,
 	[Customer_Email_Address] [nvarchar](50) NOT NULL,
 	)
+
+--Create dim location
+CREATE TABLE [ComputerStg].[dbo].[dim_location](
+	[LocationID] INT IDENTITY (1,1) PRIMARY KEY,
+	[Continent] [nvarchar](50) NOT NULL,
+	[Country_or_State] [nvarchar](50) NOT NULL,
+	[Province_or_City] [nvarchar](100) NOT NULL,
+	)
