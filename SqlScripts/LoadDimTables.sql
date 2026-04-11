0cce876aa47acddba83c78ca6a3b351f2c6da3f5
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-11 12:02:02 +0200

    Load dim store

--- SqlScripts/LoadDimTables.sql
+++ SqlScripts/LoadDimTables.sql
@@ -1,5 +1,6 @@
 --Load Dimension  tables
 
+
 --Load customer table
 INSERT INTO [ComputerStg].[dbo].[dim_customer]
 (Customer_Name,Customer_Surname,Customer_Contact_Number,Customer_Email_Address)
@@ -10,3 +11,10 @@ FROM [ComputerStg].[dbo].[raw_pc_data]
 INSERT INTO [ComputerStg].[dbo].[dim_location] (Continent,Country_or_State, Province_or_City)
 SELECT Continent,Country_or_State,Province_or_City
 FROM [ComputerStg].[dbo].[raw_pc_data]
+
+--Load dim store
+INSERT INTO [ComputerStg].[dbo].[dim_store] (Shop_Name,Shop_Age )
+SELECT Shop_Name,Shop_Age
+FROM [ComputerStg].[dbo].[raw_pc_data]
+
+
