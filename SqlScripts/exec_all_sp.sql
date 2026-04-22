bf86e3a01d833c9d03768455f151f3c5c026a4a8
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-22 19:58:09 +0200

    execute all stored procedures

---
+++ SqlScripts/exec_all_sp.sql
@@ -0,0 +1,12 @@
+
+--Execute all stored procedures
+
+EXEC dbo.usp_Create_Load_Dim_Customer
+EXEC dbo.usp_Create_Load_Dim_Location
+EXEC dbo.usp_Create_Load_Dim_Store
+EXEC dbo.usp_Create_Load_Dim_Product
+EXEC dbo.usp_Create_Load_Dim_Payment
+EXEC dbo.usp_Create_Load_Dim_Channel
+EXEC dbo.usp_Create_Load_Dim_Price
+
+EXEC dbo.usp_Create_Load_Fact_PC_Sales
