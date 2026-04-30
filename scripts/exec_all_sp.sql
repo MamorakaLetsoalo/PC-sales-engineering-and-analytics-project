76648c7b275090d46ca6fbd7b47a788c7b5e299f
MamorakaLetsoalo <mamorakaletsoalo@gmail.com>
2026-04-30 19:43:22 +0200

    update execute all

--- scripts/exec_all_sp.sql
+++ scripts/exec_all_sp.sql
@@ -1,7 +1,4 @@
-
---Execute all stored procedures
-
-EXEC dbo.usp_Create_Load_Dim_Customer
+EXEC [dbo].[usp_Create_Load_Dim_Customer]
 EXEC dbo.usp_Create_Load_Dim_Location
 EXEC dbo.usp_Create_Load_Dim_Store
 EXEC dbo.usp_Create_Load_Dim_Product
@@ -9,4 +6,7 @@ EXEC dbo.usp_Create_Load_Dim_Payment
 EXEC dbo.usp_Create_Load_Dim_Channel
 EXEC dbo.usp_Create_Load_Dim_Price
 
-EXEC dbo.usp_Create_Load_Fact_PC_Sales
+EXEC dbo.usp_Load_Fact_PC_Sales
+
+SELECT * FROM [dbo].[fact_pc_sales]
+
